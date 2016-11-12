7c421d209e2433af3b6851201c6a7e92b73bf985
rami <rami>
2016-11-12 14:10:19 +0100

    Programming assignment 2 final

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
+library(MASS)
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+# This function creates a special "matrix" object that can cache its inverse
+# 1 - create the NULL object for inverse
+# 2 - set the value of the matrix
+# 3 - get the value of the matrix
+# 4 - set the value of inverse matrix
+# 5 - get the value of inverse matrix
+makeCacheMatrix <- function(x) {
+  
+  inv <- NULL
+  get <- function() x
+  setInv <- function(inverse) inv <<- inverse
+  getInv <- function() inv
+  # create a list with function methods
+  list(get=get, setInv=setInv, getInv=getInv)
 }
 
 
-## Write a short comment describing this function
-
+# This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+# If the inverse has already been calculated (and the matrix has not changed), 
+# then the cachesolve should retrieve the inverse from the cache.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInv()
+  if(!is.null(inv)) {
+    message("matrix from cache")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- ginv(data)
+  x$setInv(inv)
+  inv
 }
+
+## Example usage:
+## > x = rbind(c(1, -1/4), c(-1/4, 1))
+## > m = makeCacheMatrix(x)
+
+## First run -no cache-
+## > cacheSolve(m)
+##           [,1]      [,2]
+## [1,] 1.0666667 0.2666667
+## [2,] 0.2666667 1.0666667
+
+## Second run -cache found in object-
+## > cacheSolve(m)
+## getting cached data.
+##           [,1]      [,2]
+## [1,] 1.0666667 0.2666667
+## [2,] 0.2666667 1.0666667
+## >
